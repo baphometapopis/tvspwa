bdcd7306e773129016b20de4693e6e95feb3425f
Joginder Yadav <joginder@indicosmic.com>
2024-01-14 19:35:16 +0530

    category id added

--- src/Api/MakerAction.js
+++ src/Api/MakerAction.js
@@ -4,7 +4,7 @@ export const makerAction = async (data) => {
   try {
     const body = new URLSearchParams();
     body.append("esclated_by_comment", data?.esclated_by_comment);
-    // body.append("esclated_by_category_id", data?.esclated_by_category_id);
+    body.append("esclated_by_category_id", data?.esclated_by_category_id);
     body.append("job_id", data?.job_id);
     body.append("user_id", data?.user_id);
 
